7b6a496211231b11132385bece6c99637ddcc06f
Willy <1319572+willyhun@users.noreply.github.com>
2026-05-08 10:33:21 +0200

    Create script to disable esp module loading
    This script unloads the esp4 and esp6 modules and blacklists them to prevent loading.

---
+++ disable_esp_module.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "unload the modules first"
+modprobe -r esp4 || echo "REBOOT_REQUIRED"
+modprobe -r esp6 || echo "REBOOT_REQUIRED"
+cat > /etc/modprobe.d/10-esp_xfrm-bug-workaround.conf <<EOF
+blacklist esp4
+blacklist esp6
+install esp4 /bin/false
+install esp4 /bin/false
+EOF
+echo "Module load disabled (if it is a module)"
