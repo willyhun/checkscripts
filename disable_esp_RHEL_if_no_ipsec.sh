aadc1ac74a071597a8deb2158ef2f806b40e3901
Willy <1319572+willyhun@users.noreply.github.com>
2026-05-08 10:49:58 +0200

    Add RHEL version
    Add RHEL version

---
+++ disable_esp_RHEL_if_no_ipsec.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rpm -q openswan 2>&1 && echo "IPSec (openswan) found exit" && exit -1
+rpm -q libreswan 2>&1 && echo "IPSec (libreswan) found exit" && exit -1
+echo "unload the modules first"
+modprobe -r esp4 || echo "REBOOT_REQUIRED"
+modprobe -r esp6 || echo "REBOOT_REQUIRED"
+cat > /etc/modprobe.d/10-esp_xfrm-bug-workaround.conf <<EOF
+blacklist esp4
+blacklist esp6
+install esp4 /bin/false
+install esp4 /bin/false
+EOF
+# if you need to
+# chattr +i /etc/modprobe.d/10-esp_xfrm-bug-workaround.conf
+echo "Module load disabled (if it is a module)"
