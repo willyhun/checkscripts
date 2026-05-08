4ef8717feb3dd71a5cacac4c09743afa0afb2a3e
Willy <1319572+willyhun@users.noreply.github.com>
2026-05-08 09:59:28 +0200

    Add message and unload module in disable_rxrpc.sh

--- disable_rxrpc.sh
+++ disable_rxrpc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "unload the module first"
+modprobe -r rxrpc || echo "REBOOT_REQUIRED"
 cat > /etc/modprobe.d/10-rxrpc-bug-workaround.conf <<EOF
 blacklist rxrpc
 install rxrpc /bin/false
